8cdf50b345e98ffc9affc352ca3dbc7cc0b8a954
Estefanny Ruíz González <tefypaoruiz@gmail.com>
2025-12-30 20:54:25 -0500

    Esta es una calculadora sencilla

--- Cuarta_clase/primer_programa.py
+++ Cuarta_clase/primer_programa.py
@@ -1,3 +1,43 @@
 ##Calculadora
 ## Va a recibir dos números pero el usuario no puede escribir cualquier cosa
 ##usar la función 'input'
+#debe tener suma, resta, división, multiplicación, división, potencia, módulo, entre dos números 
+
+try: 
+    print("Welcome to Tefy's calculator")
+    print("A continuación, ingresa dos números y elige la operación del menú de opciones:")
+    print("Menú de opciones:")
+    print("1. Suma")
+    print("2. Resta")
+    print("3. Multiplicación")
+    print("4. División")
+    print("5. Potenciación")
+    print("6. Módulo")
+    x= int(input("Introduce el primer número:"))
+    y= int(input("Introduce el 2do número:"))
+    operation= input("¿Qué operación deseas hacer?: (Selecciona una opción del anterior menú de opciones)")
+    
+
+    if operation =="1":
+        print("La suma de los dos números es:", x + y)
+    elif operation=="2":
+        print("El resultado de restar los dos números es:", x - y)
+    elif operation== "3":
+        print("El producto de los dos números es:", x ** y)
+    elif operation== "4":
+        print("El cociente de dividir los dos números es:", x / y)   
+    elif operation== "5":
+        print("La potencia es:", x ** y)    
+    elif operation=="6":
+        print("El módulo entre los dos números es:", x % y)
+    else:
+        print("Operación no válida.")
+
+except ValueError:
+    print("Hay un error. No se pueden operar números con letras")
+
+except: 
+    print("División por cero no es posible")
+
+
+
--- Tercera_clase/file.py
+++ Tercera_clase/file.py
@@ -1,5 +1,15 @@
+#Contexto global
 p= 10
 h= 20
+
 nombre= "Estefanny"
 
-def saludar ():
+def saludar (nombre): #Comienza un nuevo contexto
+    x= 15
+    print(x)
+
+    def despedir (): #nuevo contexto que vive dentro de saludar 
+        print ("Adios", x)
+
+
+    
